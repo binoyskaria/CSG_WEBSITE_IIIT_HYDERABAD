f06b75a75bb39d93527176719bc929f745951332
binoyskaria <binoy370sk@gmail.com>
2023-12-12 13:20:22 +0530

    new backend hosted

--- frontend/home/assets/js/config.js
+++ frontend/home/assets/js/config.js
@@ -1,3 +1,3 @@
 // config.js
-const apiUrl = 'https://5csglab-production.up.railway.app';
-
+// const apiUrl = 'https://5csglab-production.up.railway.app';
+const apiUrl = 'https://csg-lab-dev-hhsj.1.sg-1.fl0.io/';
