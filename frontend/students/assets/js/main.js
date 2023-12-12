b0aefd286aaa5d540c09ee096bc46ea47af5edfb
binoyskaria <binoy370sk@gmail.com>
2023-12-12 13:50:39 +0530

    loading animation reimplemented

--- frontend/students/assets/js/main.js
+++ frontend/students/assets/js/main.js
@@ -332,78 +332,6 @@ function doloading() {
 
 
 
-// // Function to fetch data from the API
-// async function fetchDataFromAPI() {
-// 	try {
-// 		console.log('Fetching data from the API...');
-// 		const response = await fetch(apiUrl + '/api/images/download/all', {
-// 			method: 'GET',
-// 			headers: {
-// 				'Content-Type': 'application/json',
-// 				'ngrok-skip-browser-warning': 1
-// 				// Add any other headers as needed
-// 			},
-// 			// credentials: 'include', // Uncomment this line if you need to include credentials (cookies, etc.)
-// 		}); // Update the URL based on your actual API endpoint
-// 		console.log('API response received:', response);
-
-// 		const data = await response.json();
-// 		console.log('Data from the API:', data);
-
-// 		return data;
-// 	} catch (error) {
-// 		console.error('Error fetching data from the API:', error);
-// 		throw error;
-// 	}
-// }
-
-// // Function to create and append articles to the DOM
-// function appendArticlesToDOM(articles) {
-// 	console.log('Appending articles to the DOM...');
-// 	const tilesSection = document.querySelector('.tiles');
-// 	let currentStyleIndex = 0;
-
-// 	articles.forEach((article) => {
-// 		const articleElement = document.createElement('article');
-// 		articleElement.classList.add(`style${currentStyleIndex + 1}`);
-
-// 		articleElement.innerHTML = `
-// 		<span class="image">
-// 		  <img src="data:image/jpeg;base64,${article.imageData}" alt="${article.title}" />
-// 		</span>
-// 		<a href="javascript:void(0);">
-// 		  <h2>${article.title}</h2>
-// 		  <div class="content">
-// 			<p>${article.description}</p>
-// 		  </div>
-// 		</a>
-// 	  `;
-
-// 		tilesSection.appendChild(articleElement);
-// 		currentStyleIndex = (currentStyleIndex + 1) % 6;
-// 	});
-
-// 	console.log('Articles appended to the DOM.');
-// }
-
-// async function fetchDataAndPopulateArticles() {
-// 	try {
-// 		console.log('Fetching data and populating articles...');
-// 		const data = await fetchDataFromAPI();
-// 		console.log('Data received:', data);
-
-// 		appendArticlesToDOM(data);
-// 	} catch (error) {
-// 		// Handle errors as needed
-// 		console.error('Error fetching data and populating articles:', error);
-// 	}
-// }
-
-// // Trigger the data fetching and population process
-// fetchDataAndPopulateArticles();
-document.addEventListener("DOMContentLoaded", function() {
-    removeloading();
-});
 
 
 function removeloading() {
@@ -426,7 +354,7 @@ function showLoadingSpinner() {
 // Function to hide the loading spinner
 function hideLoadingSpinner() {
 
-	// removeloading();
+	 removeloading();
 
 }
 
