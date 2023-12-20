48ec5d446c0e074168de482193a482f53e94eb8b
binoyskaria <binoy370sk@gmail.com>
2023-12-21 00:00:42 +0530

    switched to railway backend

--- frontend/home/assets/js/config.js
+++ frontend/home/assets/js/config.js
@@ -1,7 +1,7 @@
 // config.js
-// const apiUrl = 'https://5csglab-production.up.railway.app';
+const apiUrl = 'https://5csglab-production.up.railway.app';
 // const apiUrl = 'https://csg-lab-dev-hhsj.1.sg-1.fl0.io';
 
 // const apiUrl ='https://malamute-devoted-fully.ngrok-free.app';
 
-const apiUrl = 'https://csg-website.zeabur.app';
+// const apiUrl = 'https://csg-website.zeabur.app';
