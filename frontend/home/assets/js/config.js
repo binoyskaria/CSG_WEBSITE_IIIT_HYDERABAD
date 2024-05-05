890d99042385acd9ac5080ffb8b026b9295056a5
Binoy Skaria <100297257+binoyskaria@users.noreply.github.com>
2024-05-06 00:45:48 +0530

    Update config.js

--- frontend/home/assets/js/config.js
+++ frontend/home/assets/js/config.js
@@ -1,8 +1,8 @@
 // config.js
 // const apiUrl = 'https://5csglab-production.up.railway.app';
 // const apiUrl = 'https://csgiiit.zeabur.app';
-const apiUrl = 'https://csg-lab-dev-hhsj.1.sg-1.fl0.io';
-
+// const apiUrl = 'https://csg-lab-dev-hhsj.1.sg-1.fl0.io';
+const apiUrl = 'electric-llama-binoyskaria-256f1817.koyeb.app/';
 // const apiUrl ='https://malamute-devoted-fully.ngrok-free.app';
 
 // const apiUrl = 'https://csg-website.zeabur.app';
